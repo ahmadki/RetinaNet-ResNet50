93929ee8b4beab278fb14009d2b3488b16f71f72
Ahmad Kiswani <me@ahmadki.com>
2021-09-13 10:56:26 +0300

    Added trainings scripts

---
+++ scripts/train/300x300_8gpus.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+nvidia-smi
+env
+python -m torch.distributed.launch --nproc_per_node=8 --use_env train.py \
+    --dataset coco \
+    --model retinanet_resnet50_fpn \
+    --epochs 26 \
+    --lr-steps 16 22 \
+    --aspect-ratio-group-factor 3 \
+    --lr 0.01 \
+    --min-size=300 \
+    --max-size=300
---
+++ scripts/train/800x1300_8gpus_amp.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+nvidia-smi
+env
+python -m torch.distributed.launch --nproc_per_node=8 --use_env train.py \
+    --dataset coco \
+    --model retinanet_resnet50_fpn \
+    --epochs 26 \
+    --lr-steps 16 22 \
+    --aspect-ratio-group-factor 3 \
+    --lr 0.01 \
+    --min-size=800 \
+    --max-size=1333 \
+    --amp
