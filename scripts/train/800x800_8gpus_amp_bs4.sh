86e2531168225c4de4efd19eca12a5de42dbf6c2
Ahmad Kiswani <kiswani.ahmad@gmail.com>
2021-09-16 06:01:41 +0300

    batch size 4 configs

---
+++ scripts/train/800x800_8gpus_amp_bs4.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+nvidia-smi
+env
+python -m torch.distributed.launch --nproc_per_node=8 --use_env train.py \
+    --dataset coco \
+    --model retinanet_resnet50_fpn \
+    --epochs 26 \
+    --lr-steps 16 22 \
+    --aspect-ratio-group-factor 3 \
+    --lr 0.01 \
+    --batch-size 4 \
+    --min-size=800 \
+    --max-size=800 \
+    --amp
